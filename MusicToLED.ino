9121e92666be5d9e75a53f8b4fb74763c3cab8d4
Daniel Gorbunov <38931252+dgorbunov@users.noreply.github.com>
2020-03-27 23:25:43 -0400

    Add files via upload

---
+++ MusicToLED.ino
@@ -0,0 +1,60 @@
+/*
+  ReadAnalogVoltage
+
+  Reads an analog input on pin 0, converts it to voltage, and prints the result to the Serial Monitor.
+  Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
+  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/ReadAnalogVoltage
+*/
+#include "FastLED.h"
+
+#define NUM_LEDS 150
+#define LED_PIN 5
+#define BRIGHTNESS 255
+#define LED_TYPE WS2812B
+#define COLOR_ORDER GRB
+CRGB leds[NUM_LEDS];
+CRGBPalette16 currentPalette;
+TBlendType    currentBlending;
+
+const int threshold = 1.6;
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+   FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+    FastLED.setBrightness(  BRIGHTNESS );
+    
+    currentPalette = RainbowColors_p;
+    currentBlending = LINEARBLEND;
+
+}
+void fadeall() { for(int i = 0; i < NUM_LEDS; i++) { leds[i].nscale8(250); } }
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  float voltage = sensorValue * (5.0 / 1023.0);
+  // print out the value you read:
+  Serial.println(voltage);
+  if (voltage > threshold) {
+    fill_rainbow( &(leds[1]), random(20,150) /*led count*/, random(1,255) /*starting hue*/);
+    Serial.print("sent........................");
+    FastLED.show();
+    }
+  if (voltage < threshold) {
+    fill_solid( &(leds[1]), 150 /*number of leds*/, CRGB(0,0,0));
+    Serial.print("off<><><><><><><><><><><><>");
+    FastLED.show();
+    
+  }
+  
+}
+
+
